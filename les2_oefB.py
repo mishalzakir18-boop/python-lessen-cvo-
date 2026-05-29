dd4a6e35f917758ccf31d019cc2bb664a9c86e5d
mishalzakir18-boop <mishalzakir18@gmail.com>
2026-05-29 11:00:02 +0200

    oefC_deel1 klaar

--- les2_oefB.py
+++
@@ -1,5 +0,0 @@
-Naam = input("wat is je naam ?")
-leeftijd = int(input("wat is je leeftijd ?"))
-volgende_leeftijd = leeftijd + 1
-print(f'Hallo {Naam}! Volgend jaar ben je {volgende_leeftijd} jaar oud.')
-print(Naam.upper())
---
+++ les2_oefC.py
@@ -0,0 +1,16 @@
+# les2_oefC.py
+
+
+getal1 = int(input("Voer het eerste gehele getal in: "))
+getal2 = int(input("Voer het tweede gehele getal in: "))
+
+som = getal1 + getal2
+verschil = getal1 - getal2
+product = getal1 * getal2
+deling = getal1 / getal2
+
+
+print("Som:", som)
+print("Verschil:", verschil)
+print("Product:", product)
+print("Deling:", deling)
