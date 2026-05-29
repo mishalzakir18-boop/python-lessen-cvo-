91e10c46848920f8e56ac79631f314425b7dd03a
mishalzakir18-boop <mishalzakir18@gmail.com>
2026-05-29 11:02:07 +0200

    les1_oefC_deel2 klaar

--- les2_oefC.py
+++ les2_oefC.py
@@ -14,3 +14,28 @@ print("Som:", som)
 print("Verschil:", verschil)
 print("Product:", product)
 print("Deling:", deling)
+
+
+getal1 = int(input("Voer het eerste gehele getal in: "))
+getal2 = int(input("Voer het tweede gehele getal in: "))
+
+
+som = getal1 + getal2
+verschil = getal1 - getal2
+product = getal1 * getal2
+deling = getal1 / getal2
+kwadraat = getal1 ** 2
+
+
+if getal1 % 2 == 0:
+    soort = "even"
+else:
+    soort = "oneven"
+
+
+print("Som:", som)
+print("Verschil:", verschil)
+print("Product:", product)
+print("Deling:", deling)
+print("Kwadraat van het eerste getal:", kwadraat)
+print("Het eerste getal is:", soort)
