9ca7a210569eb2724f2ead983f2a25bf3d1a9840
mishalzakir18-boop <mishalzakir18@gmail.com>
2026-05-29 11:39:08 +0200

    oefC deel2 klaar

--- les2_factuur.py
+++ les2_factuur.py
@@ -22,3 +22,28 @@ else:
     print(f"BTW ({btw_percentage}%):      € {btw:.2f}")
     print(f"TOTAAL:          € {totaal:.2f}")
     print("===============================")
+
+    klant = input("Naam van de klant: ")
+aantal = int(input("Aantal producten: "))
+
+if aantal < 0:
+    print("FOUT: aantal producten mag niet negatief zijn.")
+else:
+    prijs = float(input("Prijs per stuk: € "))
+    btw_percentage = float(input("BTW-percentage (6, 12 of 21): "))
+
+    
+    subtotaal =round (aantal * prijs, 2) 
+    btw = round (subtotaal * (btw_percentage / 100), 2) 
+    totaal = round(subtotaal + btw, 2) 
+
+    
+    print("===============================")
+    print("Factuur voor:", klant)
+    print("Aantal producten:", aantal)
+    print(f"Prijs per stuk:  € {prijs:.2f}")
+    print("--------------------------------")
+    print(f"Subtotaal:       € {subtotaal:.2f}")
+    print(f"BTW ({btw_percentage}%):      € {btw:.2f}")
+    print(f"TOTAAL:          € {totaal:.2f}")
+    print("===============================")
